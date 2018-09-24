45ee0c801ee87d2fdaa21e0874db266c94407369
Ameen Eetemadi <ameenetemady@github.com>
2018-09-24 15:31:21 -0700

    add KerasMLPTrainPred.py

---
+++ lib/data_rw.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import os
+
+def readFoldsIdx(fold_filepath):
+    separator = ','
+    with open(fold_filepath, 'r') as bf:
+        idx_train = list(map(int,
+                         bf.readline().rstrip().split(separator)))
+        idx_test = list(map(int,
+                        bf.readline().rstrip().split(separator)))
+    
+    return idx_train, idx_test
+
+
+def readCsvRows(filename, idx_rows):
+    df_all = pd.read_csv(filename, sep="\t")
+    df_selected = df_all.iloc[idx_rows]
+
+    return df_selected.sort_index().reset_index(drop=True)
+    
+def loadData(data_filepaths, fold_filepath):
+    idx_train, idx_test = readFoldsIdx(fold_filepath)
+
+    df_train_nonTFs = readCsvRows(data_filepaths['NonTFs'], idx_train)
+    df_train_TFs = readCsvRows(data_filepaths['TFs'], idx_train)
+    df_train_KOs = readCsvRows(data_filepaths['KOs'], idx_train)
+
+    df_test_nonTFs = readCsvRows(data_filepaths['NonTFs'], idx_test)
+    df_test_TFs = readCsvRows(data_filepaths['TFs'], idx_test)
+    df_test_KOs = readCsvRows(data_filepaths['KOs'], idx_test)
+
+    return pd.concat([df_train_TFs, df_train_KOs], axis=1), df_train_nonTFs,\
+           pd.concat([df_test_TFs, df_test_KOs], axis=1), df_test_nonTFs
+
+def savePreds(df_preds, dir_path, fold_filename, layers_config, alpha, filePrefix):
+    file_prefix = '{:s}_pred_a={:.4f}_'.format(filePrefix, alpha) + 'H'.join(str(x) for x in np.array(layers_config))
+    pred_filepath = '{!s}/folds/{!s}_{!s}.csv'.format(dir_path, file_prefix, fold_filename[:-4])
+    df_preds.to_csv(pred_filepath, sep='\t', index=False)
+    print("saved: {!s}".format(pred_filepath))
+
+def get_ge_range(filename):
+    if not os.path.exists(filename):
+        print("Note: file \'{!s}\'does not exist (this maybe fine)".format(filename))
+        return None
+        
+    print("apply range using: {!s}".format(filename))
+    df = pd.read_csv(filename)
+    range_dic = {}
+    for __, row in df.iterrows():
+        range_dic[row['gene']] = [row['min'], row['max']]
+
+    return range_dic
---
+++ lib/keras_util.py
@@ -0,0 +1,100 @@
+#keras_util.py
+import keras
+from keras.callbacks import EarlyStopping
+from keras.layers import Lambda
+import numpy as np
+import math
+import random
+
+isCallHistory = False
+random.seed(1)
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+
+    def save(self, dir_path, fold_filename, layers_config, alpha, filePrefix):
+        file_prefix = '{:s}_history_a={:.4f}_'.format(filePrefix, alpha) + 'H'.join(str(x) for x in np.array(layers_config))
+        history_filepath = '{!s}/folds/{!s}_{!s}.csv'.format(dir_path, file_prefix, fold_filename[:-4])
+
+        np.savetxt(history_filepath, self.losses, delimiter=',', header="MSE", comments='')
+        print("saved: {!s}".format(history_filepath))
+
+
+def fitModel(model, input, output, layers_config, alpha, prefix, dir_path, fold_filename):
+    nM = input.shape[0]
+    rmsprop = keras.optimizers.RMSprop(lr=0.001)
+    model.compile(optimizer=rmsprop, loss='mse', metrics=['mse'])
+    
+    
+    earlystop = EarlyStopping(monitor='loss', min_delta=0.0001, patience=100, verbose=1, mode='min')
+
+    if isCallHistory:
+        history = LossHistory()
+        model.fit(input, output, epochs=10000, batch_size=nM, callbacks=[history, earlystop], verbose=1)
+        history.save(dir_path, fold_filename, layers_config, alpha, prefix)
+    else:
+        model.fit(input, output, epochs=10000, batch_size=nM, callbacks=[earlystop], verbose=0)
+
+def getMLPHyperParameters(min_nodes, max_nodes, min_alpha, max_alpha, num_combinations):
+    h_list = []
+    
+    # hidden_layers=0: 10%
+    n0 = math.ceil(num_combinations * 0.1)
+    for i in range(0, n0):
+        alpha = random.uniform(min_alpha, max_alpha)
+        h_list.append([alpha, []])
+
+    # hidden_layers=1: 20%
+    n1 = math.ceil(num_combinations * 0.2)
+    for i in range(0, n1):
+        alpha = random.uniform(min_alpha, max_alpha)
+        nodes_l1 = random.randint(min_nodes, max_nodes)
+        h_list.append([alpha, [nodes_l1]])
+
+    # hidden_layers=2: 30%
+    n2 = math.ceil(num_combinations * 0.3)
+    for i in range(0, n2):
+        alpha = random.uniform(min_alpha, max_alpha)
+        nodes_l1 = random.randint(min_nodes, max_nodes)
+        nodes_l2 = random.randint(min_nodes, max_nodes)
+        h_list.append([alpha, [nodes_l1, nodes_l2]])
+
+    # hidden_layers=3: 40%
+    n3 = num_combinations - len(h_list)
+    for i in range(0, n3):
+        alpha = random.uniform(min_alpha, max_alpha)
+        nodes_l1 = random.randint(min_nodes, max_nodes)
+        nodes_l2 = random.randint(min_nodes, max_nodes)
+        nodes_l3 = random.randint(min_nodes, max_nodes)
+        h_list.append([alpha, [nodes_l1, nodes_l2, nodes_l3]])
+
+    return h_list
+
+def getRnnHyperParameters(max_depth, min_alpha, max_alpha, num_combinations):
+    h_list = []
+    for i in range(0, num_combinations):
+        alpha = random.uniform(min_alpha, max_alpha)
+        depth = random.randint(1, max_depth)
+        h_list.append([alpha, depth])
+    
+    return h_list
+
+
+def apply_range(model, ge_range_all, output_cols):
+    mins = []
+    maxs = []
+    for col in output_cols:
+        mins.append(ge_range_all[col][0])
+        maxs.append(ge_range_all[col][1])
+    
+    mul = np.asarray(maxs) - np.asarray(mins)
+    add = np.asarray(mins)
+
+    model.add(Lambda(lambda x: x * mul))
+    model.add(Lambda(lambda x: x + add))
+
+    return model
