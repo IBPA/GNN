5fad153407ae29772fb717295130a537701a9434
Ameen Eetemadi <ameenetemady@github.com>
2018-09-24 17:12:47 -0700

    aggregate results

---
+++ vis/aggregate_per_netSize.R
@@ -0,0 +1,47 @@
+#!/usr/bin/Rscript
+library(reshape)
+source("lib_modelEval.R")
+
+getUniqPrefixes <- function(strDir, prefix, nSize){
+  fnames <- list.files(strDir, pattern = sprintf("%s.*_n%d_f[0-9]*.csv", prefix, nSize),
+                       full.names=FALSE)
+  prefixes <- lapply(fnames, function(x) unlist(strsplit(x, "_n"))[1])
+  return (unique(prefixes))
+}
+
+args=(commandArgs(TRUE))
+
+strDir <- file.path(args[1])
+#strDir <- "/home/eetemadi/mygithub/grnnPipeline/data/dream5/modules_gnw_a/size-10/"
+dirList <- paste0(list.dirs(path = strDir, full.names = TRUE, recursive = FALSE), "/d_1/folds/")
+dataSizeList <- c(10)
+#dataSizeList <- c(10)
+#prefixList <- c("grnn_pred", "biRnn_pred_a", "kmlp_pred_a", "rnn_pred_a", "avg_pred_a")
+prefixList <- c("grnn_pred")
+
+df_best_mean <- data.frame(prefix=character(), nDataSize=numeric(), MSE=numeric(), MAE=numeric(), PCC=numeric(), stringsAsFactors=FALSE )
+for(nDataSize in dataSizeList){
+   for(strPrefix in prefixList){
+      prefixes_uniq <- getUniqPrefixes(dirList[1], strPrefix, nDataSize)
+
+      strNetSizeDir <- dirList[1]
+      df <- data.frame(prefix=character(), MSE=numeric(), MAE=numeric(), PCC=numeric(), stringsAsFactors=FALSE )
+      for(strNetSizeDir in dirList){
+         for (strPrefix in prefixes_uniq) {
+               print( paste0(strNetSizeDir,"|", strPrefix, "|", nDataSize))
+               summary <- getDfDirPrefixSize(strNetSizeDir, strPrefix, nDataSize)
+               df <- rbind(df, summary)
+         }
+      }
+
+      df_aggr <- aggregate(df[,c("MSE", "MAE", "PCC" )], list(df$prefix), mean)
+      colnames(df_aggr) <- c("prefix", "MSE", "MAE", "PCC")
+      df_curr_best <- df_aggr[which.max(df_aggr[,c("PCC")]),]
+      df_curr_best$nDataSize <- nDataSize
+      df_best_mean <- rbind(df_best_mean, df_curr_best)
+   }
+}
+
+file_path <- file.path(strDir, "aggr.csv")
+write.table(df_best_mean, file=file_path, sep=",")
+print(sprintf("saved to %s", file_path))
---
+++ vis/lib_modelEval.R
@@ -0,0 +1,61 @@
+
+getDfDirPrefixSize <- function(strDir, prefix, nSize){
+  print(sprintf("%s_n%d_f[0-9]*.csv", prefix, nSize))
+  
+  filenames <- list.files(strDir, pattern = sprintf("%s_n%d_f[0-9]*.csv", prefix, nSize),
+                          full.names=TRUE)
+  fileInfo <- file.info(filenames)
+  if( nrow(fileInfo[fileInfo$size>0,]) < 5){
+     return (data.frame())
+  }
+
+  datalist <- lapply(filenames, function(x){read.csv(file=x,header=TRUE, sep = "\t")})
+  df_pred <- Reduce(function(x,y) {rbind(x,y)}, datalist)
+  df_pred_flat <- melt(df_pred)
+  
+  if (nrow(df_pred_flat) ==0){
+    print("LOOK!!!")
+  }
+  
+  filenames <- list.files(strDir, pattern = sprintf("actual_n%d_f[0-9]*.csv", nSize),
+                          full.names=TRUE)
+  datalist <- lapply(filenames, function(x){read.csv(file=x,header=TRUE, sep = "\t")})
+  df_actual <- Reduce(function(x,y) {rbind(x,y)}, datalist)
+  df_actual_flat <- melt(df_actual)
+  print(nrow(df_pred_flat))
+  print(nrow(df_actual_flat))
+
+  df <- cbind.data.frame(df_pred_flat$variable, df_pred_flat$value, df_actual_flat$value)
+  colnames(df) <- c("name", "pred", "actual")
+  
+  summary <- data.frame(prefix=prefix, 
+                        nSize=nSize, 
+                        MSE=mean((df$actual-df$pred)^2), 
+                        MAE=mean(abs(df$actual-df$pred)), 
+                        PCC=cor(df$actual, df$pred), 
+                        stringsAsFactors=FALSE )
+  return (summary)
+}
+
+getDfDirPrefix <- function(strDir, strPrefix){
+  dfOne <- data.frame(prefix=character(), nSize=integer(), MSE=numeric(), MAE=numeric(), PCC=numeric(), stringsAsFactors=FALSE )
+  for (nSize in seq(10, 100, 10)){
+    dfOne[nrow(dfOne) + 1,] <- getDfDirPrefixSize(strDir, strPrefix, nSize)
+  }
+  
+  return (dfOne)
+}
+
+
+getDfSummary <- function(df, groupBy){
+  dfMean <- aggregate(df[,c("MSE","MAE","PCC" )], list(df[,c(groupBy)]), mean)
+  colnames(dfMean) <- c(groupBy, "mean_MSE","mean_MAE","mean_PCC" )
+  dfSd <- aggregate(df[,c("MSE","MAE","PCC" )], list(df[,c(groupBy)]), sd)
+  colnames(dfSd) <- c(groupBy, "sd_MSE","sd_MAE","sd_PCC" )
+  
+  dfSummary <- cbind(dfMean[,c(groupBy)], dfMean[,c(2:4)], dfSd[,c(2:4)])
+  colnames(dfSummary)[1] <- groupBy
+  dfSummary$prefix <- df$prefix[1]
+  return(dfSummary)
+}
+
