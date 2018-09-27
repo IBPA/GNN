d7fff31a063cd1fc3e09e59fcdf43d90e09e9d70
Ameen Eetemadi <eetemadi@ucdavis.edu>
2018-09-27 15:33:34 -0700

    lasso

--- lib/keras_util.py
+++ lib/keras_util.py
@@ -34,10 +34,10 @@ def fitModel(model, input, output, layers_config, alpha, prefix, dir_path, fold_
 
     if isCallHistory:
         history = LossHistory()
-        model.fit(input, output, epochs=10000, batch_size=nM, callbacks=[history, earlystop], verbose=1)
+        model.fit(input, output, epochs=20000, batch_size=nM, callbacks=[history, earlystop], verbose=1)
         history.save(dir_path, fold_filename, layers_config, alpha, prefix)
     else:
-        model.fit(input, output, epochs=10000, batch_size=nM, callbacks=[earlystop], verbose=0)
+        model.fit(input, output, epochs=20000, batch_size=nM, callbacks=[earlystop], verbose=0)
 
 def getMLPHyperParameters(min_nodes, max_nodes, min_alpha, max_alpha, num_combinations):
     h_list = []
@@ -83,6 +83,12 @@ def getRnnHyperParameters(max_depth, min_alpha, max_alpha, num_combinations):
     
     return h_list
 
+def getLassoHyperParameters(min_alpha, max_alpha, num_combinations):
+    h_list = []
+    for i in range(0, num_combinations):
+        alpha = random.uniform(min_alpha, max_alpha)
+        h_list.append([alpha, []])
+    return h_list
 
 def apply_range(model, ge_range_all, output_cols):
     mins = []
